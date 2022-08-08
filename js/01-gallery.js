6931bc591007a3c6d9ff6bd9f7a0960ef2138213
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2022-08-09 02:08:32 +0300

    1 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listGallery = document.querySelector(".gallery");
+const createImg = createImageGallery(galleryItems);
+
+listGallery.insertAdjacentHTML("beforeend", createImg);
+
+listGallery.addEventListener("click", galleryModalOpenClose);
+
+function createImageGallery(itemList) {
+    return itemList
+      .map(({ preview, original, description }) => {
+        return `<a class="gallery__link" href="${original}">
+            <img
+            
+              class="gallery__image"
+             src="${preview}"
+               data-source="${original}"
+              alt="${description}"
+            />
+           </a>`;
+      })
+      .join("");
+  }
+
+
+    
+  function galleryModalOpenClose(event) {
+    event.preventDefault();
+  
+    const imageClickEl = event.target.classList.contains("gallery__image");
+    if (!imageClickEl) {
+      return;
+    }
+    const instance = basicLightbox.create(`
+        <img src='${event.target.dataset.source}'
+        />`);
+    instance.show();
+  
+    const CloseByEscape = function (event) {
+      if (event.code === "Escape") {
+        instance.close();
+        document.removeEventListener("keydown", CloseByEscape);
+      }
+    };
+    document.addEventListener("keydown", CloseByEscape);
+  }
