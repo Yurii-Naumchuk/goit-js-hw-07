52b492c37ecb852c64ca976de0bdd86e7be4b983
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2022-08-09 15:55:44 +0300

    2 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,38 +1,46 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const listGallery = document.querySelector(".gallery");
-const createImg = createImageGallery(galleryItems);
-
-listGallery.insertAdjacentHTML("beforeend", createImg);
-
-listGallery.addEventListener("click", galleryModalOpenClose);
-
 function createImageGallery(itemList) {
-    return itemList
-      .map(({ preview, original, description }) => {
-        return `<a class="gallery__link" href="${original}">
-            <img
+  return itemList.reduce((acc, item) => acc + 
+  `<a class="gallery__link" href="${item.original}">
+    <img class="gallery__image"
+      src="${item.preview}" data-source="${item.original}"
+      alt="${item.description}"/></a>`
+    , "");
+  };
+    // через map
+    // return itemList
+    //   .map(({ preview, original, description }) => {
+    //     return `<a class="gallery__link" href="${original}">
+    //         <img
             
-              class="gallery__image"
-             src="${preview}"
-               data-source="${original}"
-              alt="${description}"
-            />
-           </a>`;
-      })
-      .join("");
-  }
+    //           class="gallery__image"
+    //          src="${preview}"
+    //            data-source="${original}"
+    //           alt="${description}"
+    //         />
+    //        </a>`;
+    //   })
+    //   .join("");
 
 
-    
+
+  const createImg = createImageGallery(galleryItems);
+  const listGallery = document.querySelector(".gallery");
+
+  listGallery.insertAdjacentHTML("beforeend", createImg);
+  listGallery.addEventListener("click", galleryModalOpenClose);
+
+      
   function galleryModalOpenClose(event) {
     event.preventDefault();
   
     const imageClickEl = event.target.classList.contains("gallery__image");
     if (!imageClickEl) {
-      return;
-    }
+      return;}
+
+    
     const instance = basicLightbox.create(`
         <img src='${event.target.dataset.source}'
         />`);
@@ -45,4 +53,4 @@ function createImageGallery(itemList) {
       }
     };
     document.addEventListener("keydown", CloseByEscape);
-  }
+  };
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,2 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+function createImageGallery(itemList) {
+    return itemList.reduce((acc, item) => acc + 
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+      , "");
+    };
+
+    const createImg = createImageGallery(galleryItems);
+    const listGallery = document.querySelector(".gallery");
+  
+    listGallery.insertAdjacentHTML("beforeend", createImg);
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        
+        captionSelector: "img",
+        captionsData: "alt",
+        captionPosition: "bottom",
+        captionDelay: 250, });
+
+
+  
